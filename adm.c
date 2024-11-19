7e8e5efc216c7664d52a010c85b1f10ab50d67cc
1Chacon <113867659+1Chacon@users.noreply.github.com>
2024-11-19 20:13:25 -0300

    Update adm.c
    correcao de bugs

--- adm.c
+++ adm.c
@@ -4,29 +4,13 @@
 #include <time.h>
 #include "funcoes.h"
 
-// Renomeie a variável para evitar conflito
-Usuario usuarios_list[MAX_USERS]; // Mudado para 'usuarios_list'
-int num_usuarios = 0;
-
-Admin admins[MAX_ADMIN]; // Renomeado para evitar conflito
-int num_admin = 0;
-int admin_logado = -1; // Índice do administrador logado, -1 indica que ninguém está logado
-
-// Prototipagem da função login_admin
-int login_admin(char *cpf, char *senha);
 
 int main() {
-    srand(time(NULL)); // Inicializa o gerador de números aleatórios
-
-    // Chama a interface de login do administrador
-    interface_login();
-
-
-        if (interface_login) {
-        menu_admin(); // Chama o menu do admin após login bem-sucedido
+    srand(time(NULL));
+    if (interface_login()) {
+        menu_admin();
     } else {
         printf("Login falhou. Verifique seu CPF e senha.\n");
     }
-
     return 0;
 }
