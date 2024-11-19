bd2a5d97d0fdc94438ddc0326ebc67f13ff0862d
Rod-Cast <rcastanho250@gmail.com>
2024-11-19 08:02:24 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -6,45 +6,51 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_TRANSACOES 100
-#define MAX_USUARIOS 10
+#define MAX_USERS 10
+#define CPF_LENGTH 12
+#define SENHA_LENGTH 5          // 4 dígitos + 1 para o terminador nulo
+#define MAX_TRANSACOES 100      // Definindo o número máximo de transações a serem exibidas
+#define DATA_LENGTH 20          // Definindo o tamanho máximo para a data
+#define MAX_CRIPTOS 10          // Defina o número máximo de criptomoedas
+#define CRIPTO_LENGTH 50        // Defina o comprimento máximo do nome da criptomoeda
 
 typedef struct {
-    char nome[50];
-    int senha;
-    int id;
-    char cpf[12];
-    float saldo_reais;
-    float saldo_bitcoin;
-    float saldo_ethereum;
-    float saldo_ripple;
+    char cpf[CPF_LENGTH];
+    char senha[SENHA_LENGTH];
+    float saldo_reais; // Adicionando um saldo para cada usuário
 } Usuario;
 
+Usuario usuarios[MAX_USERS];
+int num_usuarios = 0;
+int user_logado = -1; // Índice do usuário logado, -1 indica que ninguém está logado
+
 typedef struct {
-    char descricao[100];
-    double valor;
-    double taxa;
-    char data[20];
+    char data[DATA_LENGTH]; // String para armazenar a data da transação
+    float valor;            // Valor da transação
+    float taxa;             // Taxa da transação
 } Transacao;
 
-typedef struct {
-    double valor_bitcoin;
-    double valor_ethereum;
-    double valor_ripple;
-} Cotacao;
 
-// Funções do projeto
-Cotacao carregar_cotacoes();
-void consultar_saldo(int i, Usuario usuarios[]);
-void consultar_extrato(int i);
-void depositar(int i, Usuario usuarios[]);
-void sacar(int i, Usuario usuarios[]);
-void comprar_criptomoedas(int i, Usuario usuarios[]);
-void vender_criptomoedas(int i, Usuario usuarios[]);
-void atualizar_cotacoes_arquivo();
-void salvar_usuarios(Usuario usuarios[], int quantidade);
-int carregar_usuarios(Usuario usuarios[]);
-int login(Usuario usuarios[]);
-void tracos();
+// Variável global para armazenar a senha do usuário logado
+char senha_usuario[SENHA_LENGTH];
+
+
+// Funções do investido
+void login_user();
+int efetuar_login(char *cpf, char *senha);
+int seguranca_user();
+void consultar_saldo();
+void consultar_extrato();
+void depositar();
+void sacar();
+void comprar_cripto();
+void vender_cripto();
+void atualizar_cotacao();
+void menu_principal();
+
+//Funcoes do admin
+
+
+
 
 #endif // PROJETO_H
